3c43c237b44e03dbfed4004472d6382dd03fdfc4
AnthonyScott <anthony.scott1005@gmail.com>
2023-05-28 08:45:31 -0400

    Initial commit

---
+++ tester.ts
@@ -0,0 +1,17 @@
+class ListNode {
+    data: number;
+    next: ListNode | null;
+
+    constructor(data: number){
+        this.data =  data;
+        this.next = null;
+    }
+
+
+}
+
+
+
+let myNode =  new ListNode(2);
+
+console.log(myNode);
