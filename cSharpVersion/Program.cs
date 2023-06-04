e50d49a3154f7b7a78483fe4a2902cf27b0072ee
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-04 17:55:33 -0400

    build a linkedlist in c#

--- cSharpVersion/Program.cs
+++ cSharpVersion/Program.cs
@@ -4,9 +4,34 @@ namespace LinkedListExample
 {
     class Program
     {
+        static Node? head;
         static void Main(string[] args)
         {
-            System.Console.WriteLine("it works!");
+            head = new Node(1);
+            head.next = new Node(2);
+            head.next.next = new Node(3);
+
+            System.Console.WriteLine(head);
+
+            Node? temp = head;
+            while (temp != null)
+            {
+                System.Console.WriteLine($"{temp.val}");
+                temp = temp.next;
+            }
+        }
+    }
+
+
+    public class Node
+    {
+        public int val;
+        public Node? next;
+
+        public Node(int data)
+        {
+            val = data;
+            next = null;
         }
     }
 }
