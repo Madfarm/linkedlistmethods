b8847ba9502ca7c1568f80bb27499988a1e00cda
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-04 17:47:51 -0400

    Set up c# version for learning

---
+++ cSharpVersion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("it works!");
+        }
+    }
+}
